7184236d83662fad27a6161d4f503935dd0baf94
Elisha <lostmatterlmao@gmail.com>
2024-08-05 16:39:31 -0500

    Testing BlockBreakEvent

---
+++ src/main/java/com/lostmatter/farmForge/listeners/CropRegrow.java
@@ -0,0 +1,22 @@
+package com.lostmatter.farmForge.listeners;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class CropRegrow implements Listener {
+
+    @EventHandler
+    public void onCropRegrow(BlockBreakEvent event) {
+        Block block = event.getBlock();
+        if (block.getType() == Material.WHEAT) {
+            event.setCancelled(true);
+            Bukkit.broadcast(Component.text("test"));
+        }
+    }
+
+}
