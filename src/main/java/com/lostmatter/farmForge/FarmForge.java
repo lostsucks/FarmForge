991465bb3d7b9feb56afa626bfdc9ad93eae5b9d
Elisha <lostmatterlmao@gmail.com>
2024-08-05 16:11:44 -0500

    Initial Commit

---
+++ src/main/java/com/lostmatter/farmForge/FarmForge.java
@@ -0,0 +1,17 @@
+package com.lostmatter.farmForge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FarmForge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
