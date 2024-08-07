4907af4f6a6f6ace86d281f58163756fa5cfea22
Elisha <lostmatterlmao@gmail.com>
2024-08-06 22:26:34 -0500

    Merge KGPatt's Code

--- src/main/java/com/lostmatter/farmForge/listeners/CropRegrow.java
+++ src/main/java/com/lostmatter/farmForge/listeners/CropRegrow.java
@@ -1,7 +1,8 @@
 package com.lostmatter.farmForge.listeners;
 
-import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.Ageable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -11,11 +12,21 @@ public class CropRegrow implements Listener {
     @EventHandler
     public void onCropRegrow(BlockBreakEvent event) {
         Block block = event.getBlock();
-        if (block.getType() == Material.WHEAT) {
-            event.setCancelled(true);
+        Ageable ageable = (Ageable) block.getBlockData();
 
-            block.setType(Material.STONE);
-            event.getPlayer().playSound(block.getLocation(), "entity.villager.happy", 1, 1);
+        if (ageable.getAge() == ageable.getMaximumAge()) {
+            event.setCancelled(true);
+            block.setType(block.getType());
+            ageable.setAge(ageable.getMaximumAge());
+
+            int locX = block.getLocation().getBlockX();
+            int locY = block.getLocation().getBlockY();
+            int locZ = block.getLocation().getBlockZ();
+
+            event.getPlayer().spawnParticle(Particle.HAPPY_VILLAGER, locX + 0.5, locY, locZ + 0.5, 5, 0.2, 0.2, 0.2, 0.03);
+            event.getPlayer().playSound(block.getLocation(), "block.note_block.pling", 1, 1);
+        } else if (ageable.getAge() < ageable.getMaximumAge()) {
+            event.setCancelled(true);
         }
     }
 
